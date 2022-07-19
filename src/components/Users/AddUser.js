32c0456d4f8f5bd25cd93d5e5a98daf210773a3f
Garosan <garosanchez27@gmail.com>
2022-07-18 21:30:48 -0500

    Add Button component, onChange controllers for inputs and log values

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log('age', age);
+    console.log('username', username);
   };
+
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleChangeAge = (e) => {
+    setAge(e.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={handleChangeUsername} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input id="age" type="number" onChange={handleChangeAge} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
