f919b83e0355b9381a628f2c3f393fe4b20dcc5e
Garosan <garosanchez27@gmail.com>
2022-07-18 20:55:46 -0500

    Bump react-scripts to v5 to avoid process is not defined erroxit exit :q

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AddUser from './components/Users/AddUser';
 
 function App() {
   return (
     <div>
-      <h1>Starting File</h1>
+      <AddUser />
     </div>
   );
 }
